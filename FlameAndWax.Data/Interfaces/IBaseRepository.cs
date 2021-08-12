770d78886fd769ba4e5b1dbd2d2db1ed361789ce
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-12 18:07:25 +0800

    Added Repository Interfaces

---
+++ FlameAndWax.Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace FlameAndWax.Data.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task Add(T Data);
+        Task Delete(int id);
+        Task Fetch(int id);
+        Task<T> FetchAll(int id);
+    }
+}
---
+++ FlameAndWax.Data/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using FlameAndWax.Data.Models;
+
+namespace FlameAndWax.Data.Interfaces
+{
+    public interface IEmployeeRepository : IBaseRepository<EmployeeModel>
+    {
+    }
+}
