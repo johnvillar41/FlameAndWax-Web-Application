f14ea1abec59878c4d1efd64b3e641aea45bcc19
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-14 21:52:59 +0800

    Added Dependency injection for services on Startup class

--- FlameAndWax/Startup.cs
+++ FlameAndWax/Startup.cs
@@ -35,6 +35,8 @@ namespace FlameAndWax
 
             //Dependency Injection Services
             services.AddSingleton<ICustomerService, CustomerService>();
+            services.AddSingleton<IEmployeeService, EmployeeService>();
+            services.AddSingleton<IAdministratorService, AdministratorService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
