34105252fbbeb8f80c3f683502dff4cf40108569
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-15 14:29:56 +0800

    Removed ErrorViewModel files

--- FlameAndWax/Controllers/HomeController.cs
+++ FlameAndWax/Controllers/HomeController.cs
@@ -1,24 +1,12 @@
-﻿using FlameAndWax.Models;
-using FlameAndWax.Services.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace FlameAndWax.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ICustomerService _customerService;
-
-        public HomeController(ILogger<HomeController> logger,ICustomerService customerService)
+    {        
+        public HomeController()
         {
-            _logger = logger;
-            _customerService = customerService;
+          
         }
 
         public IActionResult Index()
@@ -29,12 +17,6 @@ namespace FlameAndWax.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }       
     }
 }
