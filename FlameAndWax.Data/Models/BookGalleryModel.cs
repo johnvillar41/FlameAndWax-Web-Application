443602c79e1491d56aae670accec11e56f6088aa
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-18 10:51:25 +0800

    Added new Model class BookGalleryModel

---
+++ FlameAndWax.Data/Models/BookGalleryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FlameAndWax.Data.Models
+{
+    public class BookGalleryModel
+    {
+        public int BookGalleryId { get; set; }
+        public int ProductId { get; set; }
+        public string PhotoLink { get; set; }
+    }
+}
