9ed71f3ad6ab93e54ed7817d36f1a72fa62dce9a
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-18 11:18:29 +0800

    Added implementation for FetchAllPicturesForProduct()

--- FlameAndWax.Services/Repositories/ProductGalleryRepository.cs
+++ FlameAndWax.Services/Repositories/ProductGalleryRepository.cs
@@ -1,6 +1,8 @@
-﻿using FlameAndWax.Data.Models;
+﻿using FlameAndWax.Data.Constants;
+using FlameAndWax.Data.Models;
 using FlameAndWax.Services.Repositories.Interfaces;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace FlameAndWax.Services.Repositories
@@ -27,9 +29,28 @@ namespace FlameAndWax.Services.Repositories
             throw new System.NotImplementedException();
         }
 
-        public Task<IEnumerable<ProductGalleryModel>> FetchAllPicturesForProduct(int productId)
+        public async Task<IEnumerable<ProductGalleryModel>> FetchAllPicturesForProduct(int productId)
         {
-            throw new System.NotImplementedException();
+            List<ProductGalleryModel> productGalleries = new List<ProductGalleryModel>();
+
+            using SqlConnection connection = new SqlConnection(Constants.DB_CONNECTION_STRING);
+            await connection.OpenAsync();
+            var queryString = "SELECT * FROM ProductGalleryModel WHERE ProductId = @id";
+            using SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@id", productId);
+            using SqlDataReader reader = await command.ExecuteReaderAsync();
+            while(await reader.ReadAsync())
+            {
+                productGalleries.Add(
+                        new ProductGalleryModel
+                        {
+                            ProductGalleryId = int.Parse(reader["ProductGalleryId"].ToString()),
+                            ProductId = int.Parse(reader["ProductId"].ToString()),
+                            PhotoLink = reader["ProductPhotoLink"].ToString()
+                        }
+                    );
+            }
+            return productGalleries;
         }
 
         public Task Update(ProductGalleryModel data, int id)
