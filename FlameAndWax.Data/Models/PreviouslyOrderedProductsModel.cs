0254e4ac69ff194b2913ae667d79f93ae7593c34
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-20 13:34:00 +0800

    Added new Model: PreviouslyOrderedProductsModel

---
+++ FlameAndWax.Data/Models/PreviouslyOrderedProductsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FlameAndWax.Data.Models
+{
+    public class PreviouslyOrderedProductsModel
+    {
+        public int PreviouslyOrderedId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
