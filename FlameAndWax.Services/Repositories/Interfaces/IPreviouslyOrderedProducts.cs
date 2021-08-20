a026462a79c7d444b392df25db19df22ad169d83
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-20 13:36:57 +0800

    Added IPreviouslyOrderedProducts

---
+++ FlameAndWax.Services/Repositories/Interfaces/IPreviouslyOrderedProducts.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FlameAndWax.Services.Repositories.Interfaces
+{
+    public interface IPreviouslyOrderedProducts
+    {
+        Task<bool> HasCustomerOrderedAProduct(int productId,int customerId);
+    }
+}
