a332d6ceeb1e6e42282436ce6be7252486c072f0
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-20 20:26:55 +0800

    Added initial implementation of Cart

--- FlameAndWax/Cart.cs
+++ FlameAndWax/Cart.cs
@@ -1,116 +1,115 @@
-﻿//using FlameAndWax.Models;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using FlameAndWax.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace FlameAndWax
-//{
-//    public class Cart
-//    {
-//        private static readonly Dictionary<string, List<ProductViewModel>> CartItems = new Dictionary<string, List<ProductViewModel>>();
+namespace FlameAndWax
+{
+    public class Cart
+    {
+        private static readonly Dictionary<string, List<ProductViewModel>> CartItems = new Dictionary<string, List<ProductViewModel>>();
 
-//        public static void ClearCartItems()
-//        {
-//            foreach (KeyValuePair<string, List<ProductViewModel>> keyValuePair in CartItems)
-//            {
-//                //if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
-//                //{
-//                //    keyValuePair.Value.Clear();
-//                //    return;
-//                //}
-//            }
-//        }
-//        public static void AddCartItem(ProductViewModel cartProduct, string loggedInUser)
-//        {
-//            if (!CartItems.Keys.Contains(loggedInUser))
-//            {
-//                CartItems.Add(loggedInUser, new List<ProductViewModel>() { cartProduct });
-//                return;
-//            }
+        public static void ClearCartItems()
+        {
+            foreach (KeyValuePair<string, List<ProductViewModel>> keyValuePair in CartItems)
+            {
+                //if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
+                //{
+                //    keyValuePair.Value.Clear();
+                //    return;
+                //}
+            }
+        }
+        public static void AddCartItem(ProductViewModel cartProduct, string loggedInUser)
+        {
+            if (!CartItems.Keys.Contains(loggedInUser))
+            {
+                CartItems.Add(loggedInUser, new List<ProductViewModel>() { cartProduct });
+                return;
+            }
 
-//            foreach (KeyValuePair<string, List<ProductViewModel>> keyValuePair in CartItems)
-//            {
-//                if (keyValuePair.Key.Equals(loggedInUser))
-//                {
-//                    for (int i = 0; i < keyValuePair.Value.Count; i++)
-//                    {
-//                        if (keyValuePair.Value[i].ProductId == cartProduct.ProductId)
-//                        {
-//                            keyValuePair.Value[i]. = cartProduct.TotalNumberOfProduct;
-//                            return;
-//                        }
-//                        else
-//                        {
-//                            keyValuePair.Value.Add(cartProduct);
-//                            return;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        public static List<ProductViewModel> GetCartItems()
-//        {
-//            //foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
-//            //{
-//            //    if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
-//            //    {
-//            //        return keyValuePair.Value;
-//            //    }
-//            //}
-//            //return new List<ProductModel>();
-//        }
-//        public static int CalculateTotalSales()
-//        {
-//            int totalSale = 0;
-//            foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
-//            {
-//                if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
-//                {
-//                    for (int i = 0; i < keyValuePair.Value.Count; i++)
-//                    {
-//                        totalSale += (keyValuePair.Value[i].ProductPrice * keyValuePair.Value[i].TotalNumberOfProduct);
-//                    }
-//                }
-//            }
-//            return totalSale;
-//        }
-//        public static void RemoveCartItem(int productID)
-//        {
-//            foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
-//            {
-//                if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
-//                {
-//                    for (int i = 0; i < keyValuePair.Value.Count; i++)
-//                    {
-//                        if (keyValuePair.Value[i].Product_ID == productID)
-//                        {
-//                            keyValuePair.Value.Remove(keyValuePair.Value[i]);
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        public static List<OnsiteProductsTransactionModel> ListOfOnsiteProducts(int transactionID)
-//        {
-//            List<OnsiteProductsTransactionModel> OnSiteProducts = new List<OnsiteProductsTransactionModel>();
-//            foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
-//            {
-//                for (int i = 0; i < keyValuePair.Value.Count; i++)
-//                {
-//                    OnSiteProducts.Add(
-//                       new OnsiteProductsTransactionModel
-//                       {
-//                           TransactionID = transactionID,
-//                           Product = keyValuePair.Value[i],
-//                           TotalProductsCount = keyValuePair.Value[i].TotalNumberOfProduct,
-//                           Administrator = UserSession.SingleInstance.GetLoggedInUser(),
-//                           SubTotalPrice = keyValuePair.Value[i].TotalNumberOfProduct * keyValuePair.Value[i].ProductPrice
-//                       }
-//                   );
-//                }
-//            }
-//            return OnSiteProducts;
-//        }
-//    }
-//}
-//}
+            foreach (KeyValuePair<string, List<ProductViewModel>> keyValuePair in CartItems)
+            {
+                if (keyValuePair.Key.Equals(loggedInUser))
+                {
+                    for (int i = 0; i < keyValuePair.Value.Count; i++)
+                    {
+                        if (keyValuePair.Value[i].ProductId == cartProduct.ProductId)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            keyValuePair.Value.Add(cartProduct);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public static List<ProductViewModel> GetCartItems(string user)
+        {
+            foreach (KeyValuePair<string, List<ProductViewModel>> keyValuePair in CartItems)
+            {
+                if (keyValuePair.Key.Equals(user))
+                {
+                    return keyValuePair.Value;
+                }
+            }
+            return new List<ProductViewModel>();
+        }
+        //    public static int CalculateTotalSales()
+        //    {
+        //        int totalSale = 0;
+        //        foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
+        //        {
+        //            if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
+        //            {
+        //                for (int i = 0; i < keyValuePair.Value.Count; i++)
+        //                {
+        //                    totalSale += (keyValuePair.Value[i].ProductPrice * keyValuePair.Value[i].TotalNumberOfProduct);
+        //                }
+        //            }
+        //        }
+        //        return totalSale;
+        //    }
+        //    public static void RemoveCartItem(int productID)
+        //    {
+        //        foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
+        //        {
+        //            if (keyValuePair.Key.Equals(UserSession.SingleInstance.GetLoggedInUser()))
+        //            {
+        //                for (int i = 0; i < keyValuePair.Value.Count; i++)
+        //                {
+        //                    if (keyValuePair.Value[i].Product_ID == productID)
+        //                    {
+        //                        keyValuePair.Value.Remove(keyValuePair.Value[i]);
+        //                        break;
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    public static List<OnsiteProductsTransactionModel> ListOfOnsiteProducts(int transactionID)
+        //    {
+        //        List<OnsiteProductsTransactionModel> OnSiteProducts = new List<OnsiteProductsTransactionModel>();
+        //        foreach (KeyValuePair<string, List<ProductModel>> keyValuePair in CartItems)
+        //        {
+        //            for (int i = 0; i < keyValuePair.Value.Count; i++)
+        //            {
+        //                OnSiteProducts.Add(
+        //                   new OnsiteProductsTransactionModel
+        //                   {
+        //                       TransactionID = transactionID,
+        //                       Product = keyValuePair.Value[i],
+        //                       TotalProductsCount = keyValuePair.Value[i].TotalNumberOfProduct,
+        //                       Administrator = UserSession.SingleInstance.GetLoggedInUser(),
+        //                       SubTotalPrice = keyValuePair.Value[i].TotalNumberOfProduct * keyValuePair.Value[i].ProductPrice
+        //                   }
+        //               );
+        //            }
+        //        }
+        //        return OnSiteProducts;
+        //    }
+        //}
+    }
+}
--- FlameAndWax/Controllers/CartController.cs
+++ FlameAndWax/Controllers/CartController.cs
@@ -1,8 +1,8 @@
 ﻿using FlameAndWax.Data.Constants;
+using FlameAndWax.Models;
+using FlameAndWax.Services.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +10,42 @@ namespace FlameAndWax.Controllers
 {
     public class CartController : Controller
     {
-        [Authorize(Roles = nameof(Constants.Roles.Customer))]
-        public IActionResult Index(int productId, string user)
+        private readonly ICustomerService _customerService;
+        public CartController(ICustomerService customerService)
         {
-            
-            return View();
+            _customerService = customerService;
+        }
+
+        [Authorize(Roles = nameof(Constants.Roles.Customer))]
+        public async Task<IActionResult> Index(int productId = 0, string user = "")
+        {
+            if(productId == 0)
+            {
+                return View(Cart.GetCartItems(user));
+            }
+
+            var productResult = await _customerService.FetchProductDetail(productId);
+            if (productResult.HasError)
+            {
+                var error = new ErrorViewModel
+                {
+                    ErrorContent = productResult.ErrorContent
+                };
+                return View("Error", error);
+            }
+
+            var productViewModel = new ProductViewModel
+            {
+                ProductId = productId,
+                ProductName = productResult.Result.ProductName,
+                ProductDescription = productResult.Result.ProductDescription,
+                ProductPrice = productResult.Result.ProductPrice,
+                PhotoLink = productResult.Result.ProductGallery.FirstOrDefault().PhotoLink,
+                StockQuantity = productResult.Result.QuantityPerUnit * productResult.Result.UnitsInStock
+            };
+
+            Cart.AddCartItem(productViewModel, user);
+            return View(Cart.GetCartItems(user));
         }
     }
 }
