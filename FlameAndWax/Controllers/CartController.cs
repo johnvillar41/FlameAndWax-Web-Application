863c2a646d8539a6c5e23a184413c7ddb99f31f9
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-23 19:34:46 +0800

    Fix deletion of cart bugs

--- FlameAndWax/Controllers/CartController.cs
+++ FlameAndWax/Controllers/CartController.cs
@@ -35,67 +35,72 @@ namespace FlameAndWax.Controllers
         }
 
         public IActionResult DeleteCartItem(int productId)
-        {
-            Thread.Sleep(3000);
+        {            
             var userLoggedIn = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.Name).Value;
-            Cart.RemoveCartItem(productId, userLoggedIn);
-            return PartialView("CartTablePartial", Cart.GetCartItems(userLoggedIn));
+            Cart.RemoveCartItem(productId, userLoggedIn);            
+            return PartialView("CartTablePartial", new CartViewModel { CartProducts = Cart.GetCartItems(userLoggedIn) });
         }
 
         public IActionResult RefreshCart()
-        {
-            Thread.Sleep(3000);
+        {            
             var userLoggedIn = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.Name).Value;
-            return PartialView("CartTablePartial", Cart.GetCartItems(userLoggedIn));
+            return PartialView("CartTablePartial", new CartViewModel { CartProducts = Cart.GetCartItems(userLoggedIn) });
         }
+
         [HttpPost]
         public async Task<IActionResult> Checkout(CartViewModel cart)
-        {                 
-            var userLoggedInID = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.NameIdentifier).Value;
-            var userLoggedInUsername = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.Name).Value;
-
-            var cartItems = cart.CartProducts;            
-            
-            var orderDetails = new List<OrderDetailModel>();
-            
-            foreach(var cartItem in cartItems)
+        {
+            if (cart.CartProducts != null)
             {
-                var totalCost = Cart.CalculateTotalCartCost(userLoggedInUsername, cartItem.QuantityOrdered);
-                var productPriceServiceResult = await _customerService.FetchProductPrice(cartItem.ProductId);
-                if (productPriceServiceResult.HasError) return View("Error", new ErrorViewModel { ErrorContent = productPriceServiceResult.ErrorContent });
-                orderDetails.Add(
-                    new OrderDetailModel
-                    {
-                        Product = new ProductModel
+                var userLoggedInID = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.NameIdentifier).Value;
+                var userLoggedInUsername = User.Claims.FirstOrDefault(user => user.Type == ClaimTypes.Name).Value;
+               
+                var cartItems = cart.CartProducts;
+
+                var orderDetails = new List<OrderDetailModel>();
+
+                foreach (var cartItem in cartItems)
+                {                    
+                    var totalCost = Cart.CalculateTotalCartCost(userLoggedInUsername, cartItem.QuantityOrdered);
+                    var productPriceServiceResult = await _customerService.FetchProductPrice(cartItem.ProductId);
+                    if (productPriceServiceResult.HasError) return View("Error", new ErrorViewModel { ErrorContent = productPriceServiceResult.ErrorContent });
+                    orderDetails.Add(
+                        new OrderDetailModel
                         {
-                            ProductId = cartItem.ProductId,
-                            ProductPrice = productPriceServiceResult.Result //Fetch ProductPrice from service layer not from ui for safety                     
-                        },
-                        TotalPrice = totalCost,
-                        Quantity = cartItems.Count(),                        
-                    }
-                );
+                            Product = new ProductModel
+                            {
+                                ProductId = cartItem.ProductId,
+                                ProductPrice = productPriceServiceResult.Result //Fetch ProductPrice from service layer not from ui for safety                     
+                            },
+                            TotalPrice = totalCost,
+                            Quantity = cartItems.Count(),
+                        }
+                    );
+                }
+
+                var modeOfPayment = ServiceHelper.BuildModeOfPayment(cart.ModeOfPayment.ToString());
+                var courierType = ServiceHelper.BuildCourier(cart.Courier.ToString());
+                var order = new OrderModel
+                {
+                    Customer = new CustomerModel { CustomerId = int.Parse(userLoggedInID) },
+                    Employee = new EmployeeModel { EmployeeId = -1 },
+                    DateNeeded = DateTime.UtcNow,
+                    ModeOfPayment = modeOfPayment,
+                    OrderDetails = orderDetails,
+                    Courier = courierType
+                };
+                var primaryKeyServiceResult = await _customerService.InsertNewOrder(order);
+
+                if (primaryKeyServiceResult.Result == -1)
+                {
+                    return View("Error", new ErrorViewModel { ErrorContent = primaryKeyServiceResult.ErrorContent });
+                }
+
+                Cart.ClearCartItems(userLoggedInUsername);
+                return PartialView("CartTablePartial", new CartViewModel { CartProducts = Cart.GetCartItems(userLoggedInID) });
             }
 
-            var modeOfPayment = ServiceHelper.BuildModeOfPayment(cart.ModeOfPayment.ToString());
-            var courierType = ServiceHelper.BuildCourier(cart.Courier.ToString());
-            var order = new OrderModel
-            {
-                Customer = new CustomerModel { CustomerId = int.Parse(userLoggedInID) },
-                Employee = new EmployeeModel { EmployeeId = -1 },
-                DateNeeded = DateTime.UtcNow,
-                ModeOfPayment = modeOfPayment,
-                OrderDetails = orderDetails,               
-                Courier = courierType
-            };
-            var primaryKeyServiceResult = await _customerService.InsertNewOrder(order);
-            
-            if(primaryKeyServiceResult.Result == -1)
-            {
-                return View("Error", new ErrorViewModel { ErrorContent = primaryKeyServiceResult.ErrorContent });
-            }
-            Cart.ClearCartItems(userLoggedInUsername);
-            return PartialView("CartTablePartial", new CartViewModel { CartProducts = Cart.GetCartItems(userLoggedInID) });
+            return PartialView("Error", new ErrorViewModel { ErrorContent = "Empty Cart" });
         }
 
         public async Task<IActionResult> AddToCart(int productId = 0, string user = "")
