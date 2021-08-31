49145deffaf336aca232b170d76158d33733441d
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-31 11:28:41 +0800

    Moved scrips file to script.js

--- FlameAndWax/wwwroot/js/site.js
+++ FlameAndWax/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function resetText() {
+    document.getElementById("fullname").value = "";
+    document.getElementById("contactNumber").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("verifyPassword").value = "";
+    return false;
+}
 
-// Write your JavaScript code.
+completed = function () {
+    var x = document.getElementById("completed");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+    } else {
+        x.style.display = "none";
+    }
+}
+completedComment = function () {
+    var x = document.getElementById("completedComment");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+    } else {
+        x.style.display = "none";
+    }
+}
