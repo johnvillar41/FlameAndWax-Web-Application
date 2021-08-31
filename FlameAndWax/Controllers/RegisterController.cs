ee510499d4a8dff327fca7231db68b63cb9e7d0e
johnvillar41 <johndanielvillar123@gmail.com>
2021-08-31 11:58:55 +0800

    Finished implementing uploading of images on register

--- FlameAndWax/Controllers/RegisterController.cs
+++ FlameAndWax/Controllers/RegisterController.cs
@@ -1,10 +1,13 @@
 ﻿using FlameAndWax.Data.Models;
 using FlameAndWax.Models;
 using FlameAndWax.Services.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using static FlameAndWax.Data.Constants.Constants;
@@ -15,11 +18,16 @@ namespace FlameAndWax.Controllers
     {
         private readonly ICustomerService _customerService;
         private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnvironment;
         private string ConnectionString { get; set; }
-        public RegisterController(ICustomerService customerService, IConfiguration configuration)
+        public RegisterController(
+            ICustomerService customerService, 
+            IConfiguration configuration,
+            IWebHostEnvironment webHostEnvironment)
         {
             _customerService = customerService;
             _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
             ConnectionString = _configuration.GetConnectionString("FlameAndWaxDBConnection");
         }
 
@@ -38,11 +46,28 @@ namespace FlameAndWax.Controllers
                 Address = newUser.Address,
                 Username = newUser.Username,
                 Password = newUser.Password,
-                Status = CustomerAccountStatus.Pending
+                Status = CustomerAccountStatus.Pending,
+                ProfilePictureLink = await BuildProfilePictureLink(newUser.ProfilePictureFile)
             };
 
             await _customerService.Register(customerModel, ConnectionString);
             return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<string> BuildProfilePictureLink(IFormFile profilePictureFile)
+        {
+            var fileExtension = Path.GetExtension(profilePictureFile.FileName);
+            var guid = Guid.NewGuid();
+            if (fileExtension.Equals(".JPG", StringComparison.CurrentCultureIgnoreCase) ||
+                fileExtension.Equals(".PNG", StringComparison.CurrentCultureIgnoreCase) ||
+                fileExtension.Equals(".JPEG", StringComparison.CurrentCultureIgnoreCase))
+            {
+                var saveImage = Path.Combine(_webHostEnvironment.WebRootPath, @"images\customers", $"{guid}{profilePictureFile.FileName}");
+                var stream = new FileStream(saveImage, FileMode.Create);
+                await profilePictureFile.CopyToAsync(stream);
+                return @$"images\customers\{guid}{profilePictureFile.FileName}";
+            }
+            return string.Empty;
         }
     }
 }
