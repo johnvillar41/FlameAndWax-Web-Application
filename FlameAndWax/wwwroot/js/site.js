1c56155b3acdbf2d4047dee4217829137718eefe
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-01 08:40:22 +0800

    Updated site.js

--- FlameAndWax/wwwroot/js/site.js
+++ FlameAndWax/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿function resetText() {
+﻿//for clearing Contact us
+function resetText() {
     document.getElementById("fullname").value = "";
     document.getElementById("contactNumber").value = "";
     document.getElementById("email").value = "";
@@ -9,18 +10,28 @@
     return false;
 }
 
-completed = function () {
-    var x = document.getElementById("completed");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("phone").value = "";
-        document.getElementById("message").value = "";
-    } else {
-        x.style.display = "none";
+//For saving User profile
+$('#submit').click(function () {
+    completed = function (xhr) {
+        var x = document.getElementById("completed");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+            if (xhr.status == "200") {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Your profile has been saved',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        } else {
+            x.style.display = "none";
+        }
     }
-}
+});
+
+//For prompting successfull comment
 completedComment = function () {
     var x = document.getElementById("completedComment");
     if (x.style.display === "none") {
@@ -37,14 +48,7 @@ completedComment = function () {
 checkIfPasswordIsEqual = function () {
     let password = document.querySelector('#password').value;
     let verifyPassword = document.querySelector('#verifyPassword').value;
-    if (password === verifyPassword) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Your profile has been saved',
-            showConfirmButton: false,
-            timer: 1500
-        })
+    if (password === verifyPassword) {        
         return true;
     } else {
         Swal.fire({
@@ -55,4 +59,8 @@ checkIfPasswordIsEqual = function () {
         })
         return false;
     }
+}
+
+contactUsPopup = function () {
+
 }
