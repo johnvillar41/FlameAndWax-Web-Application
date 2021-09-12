a80ca981f72cddf2d3016516c1cf64d224024dfc
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-12 21:24:09 +0800

    Added Shipment Address Design on user profile

--- FlameAndWax/Controllers/RegisterController.cs
+++ FlameAndWax/Controllers/RegisterController.cs
@@ -44,7 +44,7 @@ namespace FlameAndWax.Controllers
                 CustomerName = newUser.Fullname,
                 ContactNumber = newUser.ContactNumber,
                 Email = newUser.Email,
-                Address = null,//TODO FIX THIS
+                Address = newUser.Address,//TODO FIX THIS
                 Username = newUser.Username,
                 Password = newUser.Password,
                 Status = CustomerAccountStatus.Pending,
