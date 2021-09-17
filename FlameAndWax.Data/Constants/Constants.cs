1410b6c5a64b329994807222278f5492344e5276
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-17 18:36:52 +0800

    Updated Constants and Cart Summary Design

--- FlameAndWax.Data/Constants/Constants.cs
+++ FlameAndWax.Data/Constants/Constants.cs
@@ -2,7 +2,11 @@
 {
     public class Constants
     {
-        public const string ALL_PRODUCTS = "All Products";        
+        public const string ALL_PRODUCTS = "All Products";
+        public const string GCASH_PAYMENT_DETAILS = "After clicking 'Complete Order', you will be redirected to Pay via Credit/Debit Card & PayMaya Wallet to complete your purchase securely.";
+        public const string PAYPAL_PAYMENT_DETAILS = "";
+        public const string DEBIT_CREDIT_PAYMENT_DETAILS = "";
+
         public enum Category
         {
             Candle,
@@ -26,20 +30,20 @@
         {
             Deactivated,
             Activated
-        }        
+        }
         public enum ModeOfPayment
         {
             Cash,
             GCash,
             DebitCreditCard,
             PayPal
-        }                        
+        }
         public enum OrderStatus
         {
             Pending,
             Processing,
             Shipping,
-            Finished,                       
+            Finished,
             Cancelled
         }
         public enum ReviewScore
