1e3fee015af9e28f65f670378b4e62fe636d6a3f
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-23 18:50:35 +0800

    Added initial design for HomePage and Added Account Controller

---
+++ FlameAndWax.Employee/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlameAndWax.Employee.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
