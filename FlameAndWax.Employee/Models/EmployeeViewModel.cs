288120faa50478bb2f1414406907c8951f964ab1
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-23 14:27:30 +0800

    Added EmployeeViewModel

---
+++ FlameAndWax.Employee/Models/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlameAndWax.Employee.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhotoLink { get; set; }
+        public Date DateBirth { get; set; }
+        public Date HireDate { get; set; }
+        public string City { get; set; }
+        public string Status { get; set; }
+    }
+}
