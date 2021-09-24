9d505c167bb6011d37eac98b5ba46a1d109fd5ca
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-24 10:43:18 +0800

    Redesigned Login Page on Employee, Added Process Login and form action

--- FlameAndWax.Employee/Controllers/AccountController.cs
+++ FlameAndWax.Employee/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlameAndWax.Employee.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlameAndWax.Employee.Controllers
@@ -11,6 +12,13 @@ namespace FlameAndWax.Employee.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ProcessLogin(LoginViewModel login)
+        {
+            throw new NotImplementedException();
         }
     }
 }
