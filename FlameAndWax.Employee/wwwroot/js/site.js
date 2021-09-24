596bf3144846a2ea29a0a51414430a5c268c8e0a
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-24 13:46:29 +0800

    Added sweetalert on Employee

--- FlameAndWax.Employee/wwwroot/js/site.js
+++ FlameAndWax.Employee/wwwroot/js/site.js
@@ -1,7 +1,28 @@
 ﻿loginComplete = function (xhr) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    });
     if (xhr.status == "200") {
-        console.log("Success! nigga");
+        Toast.fire({
+            icon: 'success',
+            title: '<span style="color: #006400"><b>Success</b></span> Login Successfull!',
+            background: '#CCFFCC',
+            iconColor: '#006400',
+        });
     } else if (xhr.status == "400") {
-        console.log("Error login!");
+        Toast.fire({
+            icon: 'error',
+            title: '<span style="color: #8b0000"><b>Error!</b></span> User not found!',
+            background: '#FF7F7F',
+            iconColor: '#8b0000',
+        });
     }
 }
